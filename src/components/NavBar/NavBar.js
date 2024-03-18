b3d13ad72379c201d2e551022b100b13d838173c
Shrijit_01 <shrijitpandey113@gmail.com>
2024-03-19 00:39:08 +0530

    oneTools

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         {/* <a className="navbar-brand" href="/">Utility App</a> */}
-        <Link className="navbar-brand" to="/">Utility App</Link>
+        <Link className="navbar-brand" to="/">One Tools</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
